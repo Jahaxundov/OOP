10d9c6ee17b39d4f23dfdd7a9f29ee77bd80704f
Jahaxundov <jahangirea@code.edu.az>
2023-07-25 01:30:25 +0400

    HomeWork added

---
+++ HomeWork24.07.23/HomeWork24.07.23/CustomMath.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork24._07._23
+{
+	internal class CustomMath
+	{
+		public int SumOfOddNumbersFromArray(int[] arr)
+		{
+			int sum = 0;
+			foreach (var item in arr)
+			{
+				if (item % 2 == 1)
+				{
+					sum += item;
+				}
+			}
+			return sum;
+		}
+
+        public int ActionWithEvenNumbersFromArray(int[] arr)
+		{
+			int sum = 0;
+			int result = 1;
+			
+			foreach (var item in arr)
+			{
+				if (item % 2 == 0)
+				{
+					sum+=item;
+				}
+			}
+				result = sum * sum;
+			    return result;
+		}
+
+		public void Check(int n)
+		{
+			if (n % 2 == 0)
+			{
+				Console.WriteLine("Regem cutdur");
+			}
+			else
+			{
+                Console.WriteLine("Regem tekdir");
+            }   
+		}
+		public int GetFactorialOfNumber(int n)
+		{
+			int multiplication = 1;
+			for (int i = 1; i < n; i++)
+			{
+				multiplication *= i;
+			}
+			return multiplication;
+		}
+	}
+}
---
+++ HomeWork24.07.23/HomeWork24.07.23/Program.cs
@@ -0,0 +1,34 @@
+﻿using HomeWork24._07._23;
+
+//1)Method array qebul edir, arrayin icerisindeki tek ededlerin cemini tapin.
+
+//int[] array = { 7, 2, 5, 4, 5 };
+//CustomMath customMath = new CustomMath();
+//int result=customMath.SumOfOddNumbersFromArray(array);
+//Console.WriteLine(result);
+
+//3) Methoda gelen arrayin elementlerininden cut olanlarinin ceminin kvadratini tapmaq.
+
+//int[] array = { 2, 2, 5, 4, 2 };
+//CustomMath customMath = new CustomMath();
+//int result = customMath.ActionWithEvenNumbersFromArray(array);
+//Console.WriteLine(result);
+
+//2)Methoda gelen ededin tek ve ya cut olub olmamasini gosterin.
+
+//Console.WriteLine("N regemi daxil edin");
+//int n = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("-------------");
+//CustomMath customMath = new CustomMath();
+//customMath.Check(n);
+
+//4) Methoda n ededi gelir, 1-den n ededine qeder olan ededlerin hasilini tapin.
+
+//Console.WriteLine("N regemi daxil edin");
+//int n = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("-------------");
+//CustomMath customMath = new CustomMath();
+//int result = customMath.GetFactorialOfNumber(n);
+//Console.WriteLine(result);
+
+
